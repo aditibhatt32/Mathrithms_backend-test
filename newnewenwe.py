c8abae8929e52a3b32f4f4568fb5828d1f7ff978
aditibhatt32 <67057405+aditibhatt32@users.noreply.github.com>
2021-09-13 12:37:27 +0530

    django_test

---
+++ newnewenwe.py
@@ -0,0 +1,90 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+import pandas as pd
+import json
+app = Flask(__name__)
+api = Api(app)
+
+
+request_details = [
+    {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret",
+        "email": "Sincere@april.biz",
+        "address": {
+            "street": "Kulas Light",
+            "suite": "Apt. 556",
+            "city": "Gwenborough",
+            "zipcode": "92998-3874"
+        }
+    },
+    {
+        "id": 2,
+        "name": "Ervin Howell",
+        "username": "Antonette",
+        "email": "Shanna@melissa.tv",
+        "address": {
+            "street": "Victor Plains",
+            "suite": "Suite 879",
+            "city": "Wisokyburgh",
+            "zipcode": "90566-7771"
+        }
+    },
+    {
+        "id": 3,
+        "name": "Clementine Bauch",
+        "username": "Samantha",
+        "email": "Nathan@yesenia.net",
+        "address": {
+            "street": "Douglas Extension",
+            "suite": "Suite 847",
+            "city": "McKenziehaven",
+            "zipcode": "59590-4157"
+        }
+    },
+    {
+        "id": 4,
+        "name": "Patricia Lebsack",
+        "username": "Karianne",
+        "email": "Julianne.OConner@kory.org",
+        "address": {
+            "street": "Hoeger Mall",
+            "suite": "Apt. 692",
+            "city": "South Elvis",
+            "zipcode": "53919-4257"
+        }
+    },
+    {
+        "id": 5,
+        "name": "Chelsey Dietrich",
+        "username": "Kamren",
+        "email": "Lucio_Hettinger@annie.ca",
+        "address": {
+            "street": "Skiles Walks",
+            "suite": "Suite 351",
+            "city": "Roscoeview",
+            "zipcode": "33263"
+        }
+    },
+]
+
+class Users(Resource):
+    def get(self, request_details):
+        
+        response = fetch_details(request_details)
+        return response
+
+
+def fetch_details(request_details):
+   
+    userId = request_details.get("id")
+    user_data = Users().fetch_by_id(params=userId)
+    if not user_data:
+        print("not found")
+        return user_data
+# Add URL endpoints
+api.add_resource(Users, '/users')
+
+if __name__ == '__main__':
+    app.run()
